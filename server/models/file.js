919b51f74d9c4543e4d14849e1bff3696a368c76
doitunglam1 <doitunglam1@gmail.com>
2023-05-09 13:22:32 +0700

    file upload & retrieval

---
+++ server/models/file.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs'); // require bcryptjs here
+const jwt = require('jsonwebtoken'); 
+
+const fileSchema = new mongoose.Schema({
+    URL: {
+        type: String, 
+        required: false,
+        unique: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true });
+
+const File = mongoose.model('File', fileSchema);
+
+module.exports = File;
+
--- server/routers/test.js
+++ server/routers/test.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const sendMail = require('../services/mailer');
+const { auth } = require('../middlewares/auth');
+const { uploadFile, getFile } = require('../services/fileUpload');
+const { ROOT_ABSOLUTE } = require('../configs');
 const testRouter = express.Router();
 
 
 testRouter.post('/test/mail', async (req, res) => {
-        const { subject, content, recceiver } = req.body;
-        const mail = {
-            subject: subject,
-            content: content
-        };
-        
-        const returnMessage = await sendMail(mail, recceiver);
+    const { subject, content, recceiver } = req.body;
+    const mail = {
+        subject: subject,
+        content: content
+    };
 
-        console.log(returnMessage);
-        res.status(200).json({message:returnMessage});
-        
+    const returnMessage = await sendMail(mail, recceiver);
+
+    console.log(returnMessage);
+    res.status(200).json({ message: returnMessage });
+
+})
+
+testRouter.post('/test/files', auth, async (req, res) => {
+    const file = req.files.demo;
+
+    const message = await uploadFile(file, req.user);
+    res.status(200).json({ message: message });
+})
+// Add authentication middleware if needed
+testRouter.get('/test/files/:id',/*auth, */ async (req, res) => {
+    const file = req.params.id;
+
+    const message = await getFile(file, req.user);
+
+    if (message.status == 200)
+        res.sendFile(message.path,{ root: ROOT_ABSOLUTE });
+    else
+        res.status(message.status).json({ message: message.text });
 })
 
 module.exports = testRouter;
