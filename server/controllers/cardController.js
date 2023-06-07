1aeed170756b7008e5d053adb525461625a5f944
lamit1 <nhl9a1vn@gmail.com>
2023-06-07 13:43:35 +0700

    update, delete card

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -12,6 +12,31 @@ const createCard = async (req, res) => {
   }
 };
 
+const updateCard = async (req, res) => {
+  try {
+    const { cardId } = req.params;
+    const { cardTitle, description } = req.body;
+    const card = await Card.findByIdAndUpdate(cardId, { cardTitle, description }, { new: true });
+    res.status(200).json(card);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+const deleteCard = async (req, res) => {
+  try {
+    const { cardId } = req.params;
+    await Card.findByIdAndDelete(cardId);
+    res.status(200).json({ message: 'Card deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
 module.exports = {
-  createCard
+  createCard,
+  updateCard,
+  deleteCard
 };
--- server/routers/card.js
+++ server/routers/card.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middlewares/auth');
-const { createCard } = require('../controllers/cardController');
+const { createCard, updateCard, deleteCard } = require('../controllers/cardController');
 
 // Create route for creating a new card
 router.post('/boards/:boardId/lists/:listId/create-card', auth, createCard);
+
+// Create route for updating a card
+router.put('/cards/:cardId/update-card', auth, updateCard);
+
+// Create route for deleting a card
+router.delete('/cards/:cardId/delete-card', auth, deleteCard);
 
 module.exports = router;
