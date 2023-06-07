ea66451c3ee18a6612c9b1bf10404eb09cee682a
lamit1 <nhl9a1vn@gmail.com>
2023-06-07 13:57:44 +0700

    change login

--- client/src/pages/login_page/login.jsx
+++ client/src/pages/login_page/login.jsx
@@ -22,11 +22,9 @@ const Login = (props) => {
             const token = response.data.token;
             Cookies.set('token', token, { expires: 7 }); // Expires in 7 days
             //Routing to the profile page
-            navigate('/');
+            navigate('/boards');
             const tokenCookie = Cookies.get('token');
             console.log(tokenCookie);
-
-
           } catch (error) {
             // Handle any error that occurred during the API request
             alert("Login Failed!");
